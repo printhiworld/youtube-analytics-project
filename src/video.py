2ea8cf5d6fe82e3f8f8201d022d09ef1961996d3
printhiworld <e.muhina@it-park.tech>
2024-02-01 14:06:21 +0300

    hw

--- src/video.py
+++ src/video.py
@@ -3,14 +3,22 @@ from googleapiclient.discovery import build
 
 class Video:
     def __init__(self, video_id):
-        self.__video_id = video_id
-        self.title = self.get_info()['items'][0]['snippet']['title']
-        self.customUrl = f'https://www.youtube.com/watch?v={video_id}'
-        self.views = self.get_info()['items'][0]['statistics']['viewCount']
-        self.likes = self.get_info()['items'][0]['statistics']['likeCount']
+        try:
+            self.__video_id = video_id
+            self.title = self.get_info()['items'][0]['snippet']['title']
+            self.customUrl = f'https://www.youtube.com/watch?v={video_id}'
+            self.views = self.get_info()['items'][0]['statistics']['viewCount']
+            self.likes = self.get_info()['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.__video_id = video_id
+            self.title = None
+            self.customUrl = None
+            self.views = None
+            self.likes = None
 
     def get_info(self) -> str:
         api_key = 'AIzaSyCemWJ_acfZl3wE85blGxwZ3SkCwCTRGsk'
+
         youtube = build('youtube', 'v3', developerKey=api_key)
         video_id = self.__video_id
         video_response = youtube.videos().list(part='snippet,statistics', id=video_id).execute()
